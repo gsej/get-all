c463e4b79231efd08f422e2765ab50173180f409
gsej <gsej@erskinejones.com>
2023-02-26 10:03:46 +0000

    initial clone script

---
+++ clone.sh
@@ -0,0 +1,18 @@
+git clone git@github.com:gsej/tryout-projects.git
+git clone git@github.com:gsej/polytunnel-monitor.git
+git clone git@github.com:gsej/experiment.git
+git clone git@github.com:gsej/training.git
+git clone git@github.com:gsej/share-prices.git
+git clone git@github.com:gsej/youinvest-csv-files.git
+git clone git@github.com:gsej/dotfiles.git
+git clone git@github.com:gsej/youinvest.git
+git clone git@github.com:gsej/notesà.git
+git clone git@github.com:gsej/osinstall.git
+git clone git@github.com:gsej/internet-monitor.git
+git clone git@github.com:gsej/coding-exercise.git
+git clone git@github.com:gsej/learning.git
+git clone git@github.com:gsej/code-samples.git
+git clone git@github.com:gsej/gsej.github.io.git
+git clone git@github.com:gsej/concatenate-code.git
+git clone git@github.com:gsej/PSDevTools.git
+
