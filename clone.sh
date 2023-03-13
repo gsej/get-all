681719c0bf7a908927f5064343e13af4d39a9b37
gsej <gsej@erskinejones.com>
2023-03-13 22:10:47 +0000

    add dailycodingproject repo

--- clone.sh
+++ clone.sh
@@ -1,18 +1,17 @@
-git clone git@github.com:gsej/tryout-projects.git
-git clone git@github.com:gsej/polytunnel-monitor.git
-git clone git@github.com:gsej/experiment.git
-git clone git@github.com:gsej/training.git
-git clone git@github.com:gsej/share-prices.git
-git clone git@github.com:gsej/youinvest-csv-files.git
-git clone git@github.com:gsej/dotfiles.git
-git clone git@github.com:gsej/youinvest.git
-git clone git@github.com:gsej/notesà.git
-git clone git@github.com:gsej/osinstall.git
-git clone git@github.com:gsej/internet-monitor.git
-git clone git@github.com:gsej/coding-exercise.git
-git clone git@github.com:gsej/learning.git
-git clone git@github.com:gsej/code-samples.git
-git clone git@github.com:gsej/gsej.github.io.git
-git clone git@github.com:gsej/concatenate-code.git
-git clone git@github.com:gsej/PSDevTools.git
-
+[ ! -d ../repos/code-samples ] && git clone git@github.com:gsej/code-samples.git ../repos/code-samples
+[ ! -d ../repos/code-exercises ] && git clone git@github.com:gsej/coding-exercise.git ../repos/code-exercises
+[ ! -d ../repos/coding-exercise ] && git clone git@github.com:gsej/coding-exercise.git ../repos/coding-exercise
+[ ! -d ../repos/concatenate-code ] && git clone git@github.com:gsej/concatenate-code.git ../repos/concatenate-code
+[ ! -d ../repos/dotfiles ] && git clone git@github.com:gsej/dotfiles.git ../repos/dotfiles
+[ ! -d ../repos/gsej.github.io ] && git clone git@github.com:gsej/gsej.github.io.git ../repos/gsej.github.io	
+[ ! -d ../repos/internet-monitor ] && git clone git@github.com:gsej/internet-monitor.git ../repos/internet-monitor
+[ ! -d ../repos/notes ] && git clone git@github.com:gsej/notes.git ../repos/notes
+[ ! -d ../repos/osinstall ] && git clone git@github.com:gsej/osinstall.git ../repos/osinstall
+[ ! -d ../repos/play ] && git clone git@github.com:gsej/play.git ../repos/play
+[ ! -d ../repos/polytunnel-monitor ] && git clone git@github.com:gsej/polytunnel-monitor.git ../repos/polytunnel-monitor	
+[ ! -d ../repos/PSDevTools ] && git clone git@github.com:gsej/PSDevTools.git ../repos/PSDevTools
+[ ! -d ../repos/share-prices ] && git clone git@github.com:gsej/share-prices.git ../repos/share-prices
+[ ! -d ../repos/youinvest ] && git clone git@github.com:gsej/youinvest.git ../repos/youinvest
+[ ! -d ../repos/youinvest-csv-files ] && git clone git@github.com:gsej/youinvest-csv-files.git ../repos/youinvest-csv-files
+[ ! -d ../repos/backup ] && git clone git@github.com:gsej/backup.git ../repos/backup
+[ ! -d dailycodingproblems ] && git clone git@github.com:gsej/dailycodingproblems.git ../repos/dailycodingproblems
